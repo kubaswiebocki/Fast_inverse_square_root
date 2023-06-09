23829c4c839185473d70e8e30c52e3bc3ac72079
ognien <96772993+ognien@users.noreply.github.com>
2023-06-09 18:05:06 +0200

    [upd] Updated python file

--- scripts/MyTest2.py
+++ scripts/MyTest2.py
@@ -1,7 +1,20 @@
 import serial
 import time
 import struct
+import binascii
 
+def bytes_to_u32(byte_data):
+    # Dopasuj rozmiar bajtów do 4 bajtów (32 bitów)
+    while len(byte_data) % 4 != 0:
+        byte_data = b'\x00' + byte_data
+
+    # Konwertuj bajty na u32
+    u32_list = []
+    for i in range(0, len(byte_data), 4):
+        u32_value = int.from_bytes(byte_data[i:i+4], byteorder='big', signed=False)
+        u32_list.append(u32_value)
+    
+    return u32_list
 
 def float_to_ieee754(num):
     # Konwersja liczby zmiennoprzecinkowej na standard IEEE 754
@@ -12,24 +25,26 @@ def float_to_ieee754(num):
 # Ustawienia portu szeregowego
 port = "COM4"
 baudrate = 115200
-DataTest = [5,2,3,3.14,4,1,2,2,2,2]
+DataTest = []
+for i in range(1500):
+    DataTest.append(i+1)
 
 try:
     # Utwórz obiekt Serial do komunikacji
     ser = serial.Serial(port, baudrate, timeout=1)
-    print("Nawiązano połączenie przez port szeregowy", port)
-    while True:  
-        for i in DataTest:
-            mystr = float_to_ieee754(i).split()
-            for i in mystr:
-                #print(i)
-                ser.write(i.encode())
-            time.sleep(0.01)
+    print("Nawiązano połączenie przez port szeregowy", port) 
+    for i in DataTest:
+        ser.write(i.to_bytes(4,"big"))
+        #ser.write(i)
 
-        # Odczytaj dane z portu szeregowego
-        if ser.in_waiting > 0:
-            odebrane_dane = ser.readline().decode().strip()
-            print(odebrane_dane)
+    for i in range(1500):
+        odebrane_dane = ser.read(4)
+        #o = odebrane_dane.decode()
+        print(i, ": ", bytes_to_u32(odebrane_dane))
+        # b = int.from_bytes(o,"big")
+        # s = bin(b)
+        # print(i, ": ", s)
+       #print(str(odebrane_dane)[2:6])
 
 except serial.SerialException as e:
     print("Błąd portu szeregowego:", str(e))
