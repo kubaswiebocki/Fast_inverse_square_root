c5c1d207b609beb8871045de5a07aeceeae9b226
ognien <96772993+ognien@users.noreply.github.com>
2023-06-09 18:05:54 +0200

    [upd] Main.c in progress

--- SDK_files/src/main.c
+++ SDK_files/src/main.c
@@ -1,160 +1,43 @@
-/*
- * main.c: find_mx application
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
-//Initialize main
-//***************************************************************************//
-#include <stdio.h>
-#include "platform.h"
-#include "xil_printf.h"
-#include "str_acc.h"
+#include <stdlib.h>
+#include "xil_types.h"
 #include "xuartps.h"
+#include "sleep.h"
 
-#define NBR_OF_DATA 11 //All possible values for the range
+#define dataSize 1500*4
 
-u32 data_in[NBR_OF_DATA]; //Accelerator input buffer
-u32 data_out[NBR_OF_DATA]; //Accelerator output buffer
-float data_in_c[NBR_OF_DATA];
-float data_out_c[NBR_OF_DATA];
-float init_d = 0.0001;
-char line[32];
 
-//***************************************************************************//
-
-//Functions
-//***************************************************************************//
-float Q_rsqrt( float number )
-{
-	long i;
-	unsigned long r;
-	float x2, y;
-	const float threehalfs = 1.5F;
-
-	x2 = number * 0.5F;
-	y  = number;
-	i  = * ( long * ) &y;
-	i  = 0x5f3759df - ( i >> 1 );
-	y  = * ( float * ) &i;
-	y  = y * ( threehalfs - ( x2 * y * y ) );
-	r  = * ( long * ) &y;
-	return r;
-}
-u32 read3DigitDecVal(){
-u32 ret = 0;
-char8 c;
-    outbyte ( c = inbyte() );
-    printf("%c",inbyte);
-    ret += 100 * (c - '0');
-
-    outbyte ( c = inbyte() );
-    printf("%c",inbyte);
-    ret += 10 * (c - '0');
-
-	outbyte ( c = inbyte() );
-	printf("%c",inbyte);
-	ret += (c - '0');
-    return ret;
-}
-uint32_t floatToBits(float num) {
-    union {
-        float f;
-        uint32_t bits;
-    } converter;
-
-    converter.f = num;
-    return converter.bits;
-}
-float bitsToFloat(uint32_t bits) {
-    union {
-        float f;
-        uint32_t bits;
-    } converter;
-
-    converter.bits = bits;
-    return converter.f;
-}
-float uint32ToFloat(uint32_t bits) {
-    union {
-        float f;
-        uint32_t bits;
-    } converter;
-
-    converter.bits = bits;
-    return converter.f;
-}
-
-int fromBinary(const char *s) {
-  return (int) strtol(s, NULL, 2);
-}
-
-void CompareData(float DataIn, float DataInC, int i){
-	float floatData, floatDataC, diff;
-	floatData = bitsToFloat(DataIn);
-	floatDataC = bitsToFloat(Q_rsqrt(DataInC));
-	diff = floatDataC - floatData;
-	if(diff < 0) diff = floatData - floatDataC;
-
-	if(diff != 0){
-		printf("FAILED: ");
-		printf(" -> Sample: %.4f", (0.01 +  init_d*(i-1)));
-		printf(" -> Verilog: %.10f",floatData);
-		printf(" -> C_algorithm: %.10f",floatDataC);
-		printf(" -> Diff: %.10f\n\r",diff);
-		}
-	else{
-		printf("PASSED: ");
-		printf(" -> Sample: %.4f\n\r", (0.01 +  init_d*(i-1)));
-		}
-
-	//return 0;
-}
-//***************************************************************************//
-
-//main
-//***************************************************************************//
 int main(){
-	reset_fisr_acc();
-	cleanup_platform();
-	init_platform();
-	if ( init_fisr_acc() == XST_FAILURE )
-		goto error;
+	u8 *inputData;
+	u32 status;
+	u32 receivedBytes = 0;
+	u32 totalReceivedBytes = 0;
+	u32 totalTransmittedBytes = 0;
+	u32 transmittedBytes = 0;
+	XUartPs_Config *myUartConfig;
+	XUartPs myUart;
 
-	while(1){
-	//for(int v=0; v<10000000; v++){;}
-	u32 i, nbr_of_results;
-	float floatData; //floatDataC, diff;
-//FIFO and FPGA algorithm
-//*****************************//
-
-	for(i=0; i<10; i++){
-		fgets(line, sizeof(line), stdin);
-		u32 bits = fromBinary(line);
-		printf("%f\n\r", uint32ToFloat(bits));
-		//printf("\n\r");
-		data_in[i] = bits;
+	inputData = malloc(sizeof(u8)*dataSize);
+	myUartConfig = XUartPs_LookupConfig(XPAR_PS7_UART_1_DEVICE_ID);
+	status = XUartPs_CfgInitialize(&myUart, myUartConfig, myUartConfig->BaseAddress);
+	if(status != XST_SUCCESS){
+		print("Uart initialization failed...\n\n");
 	}
-	fisr_calc(data_in, NBR_OF_DATA, data_out, &nbr_of_results );
-
-//*****************************//
-
-//Generate data_out
-//*****************************//
-	for(i=1; i<nbr_of_results; i++){
-		floatData = bitsToFloat(data_out[i]);
-		printf(" -> Verilog: %.10f \n\r",floatData);
-		}
-	printf("Processing done\n\r");
-	cleanup_platform();
-	init_platform();
-
+	status = XUartPs_SetBaudRate(&myUart, 115200);
+	if(status != XST_SUCCESS){
+		print("Baudrate init failed....\n\n");
 	}
-	error:
-		reset_fisr_acc();
-		cleanup_platform();
+	while(totalReceivedBytes < dataSize){
+		receivedBytes = XUartPs_Recv(&myUart,(u8*)&inputData[totalReceivedBytes],100);
+		totalReceivedBytes += receivedBytes;
+	}
+
+//	for(int i=0; i<dataSize; i++){
+//		xil_printf("%x", inputData[i]);
+//	}
+	while(totalTransmittedBytes < dataSize){
+		transmittedBytes = XUartPs_Send(&myUart, (u8*)&inputData[totalTransmittedBytes], 4);
+		totalTransmittedBytes += transmittedBytes;
+		usleep(1000);
+	}
+
 }
