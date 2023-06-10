331e66b9b8e43a82ccb6df7a1b7672425ec6439e
ognien <96772993+ognien@users.noreply.github.com>
2023-06-10 23:14:59 +0200

    [feat] Integreted application with FPGA communication

--- PythonApp/FigureMoveIn3D/main.py
+++ PythonApp/FigureMoveIn3D/main.py
@@ -1,5 +1,7 @@
 import pygame
 import math
+import serial
+import struct
 
 pygame.init()
 
@@ -9,7 +11,7 @@ HEIGHT = 600
 BG_COLOR = (255, 255, 255)
 
 OBJECT_SIZE = 350
-OBJECT_COLOR_START = (255, 0, 0)
+OBJECT_COLOR_START = (0, 255, 0)
 OBJECT_COLOR_END = (10, 10, 10)
 FOCAL_LENGTH = 400
 
@@ -21,17 +23,70 @@ pygame.display.set_caption("Sterowanie obiekt z wykorzystaniem Fast Inverse Squa
 clock = pygame.time.Clock()
 
 # Functions
+
+def bytes_to_u32(byte_data):
+    # Dopasuj rozmiar bajtów do 4 bajtów (32 bitów)
+    while len(byte_data) % 4 != 0:
+        byte_data = b'\x00' + byte_data
+
+    # Konwertuj bajty na u32
+    u32_list = []
+    for i in range(0, len(byte_data), 4):
+        u32_value = int.from_bytes(byte_data[i:i+4], byteorder='big', signed=False)
+        u32_list.append(u32_value)
+    
+    return u32_list
+def float_to_ieee754(num):
+    binary = struct.pack('>f', num)  #
+    bits = int.from_bytes(binary, 'big')
+    return bits
+def int32_to_float(int_value):
+    byte_string = struct.pack('!i', int_value)
+    float_value = struct.unpack('!f', byte_string)[0]
+    return float_value
+def InverSquareRoot(DataTest):
+    port = "COM4"
+    baudrate = 115200
+    DataTest.append(1)
+    times = len(DataTest)
+    x = []
+    try:
+        ser = serial.Serial(port, baudrate, timeout=1)
+        ser.write(times.to_bytes(4,"big"))
+        
+        for i in DataTest:
+            ser.write(float_to_ieee754(i).to_bytes(4,"big"))
+
+        for i in range(times):
+            odebrane_dane = ser.read(4)
+            try:
+                x.append(int32_to_float(bytes_to_u32(odebrane_dane)[0]))
+            except:
+                pass
+
+    except serial.SerialException as e:
+        print("Błąd portu szeregowego:", str(e))
+        return x[1:]
+    except KeyboardInterrupt:
+        pass
+        return x[1:]
+    finally:
+        if ser and ser.is_open:
+            ser.close()
+        return x[1:]
+
 def interpolate_color(start_color, end_color, t):
     r = int(start_color[0] * (1 - t) + end_color[0] * t)
     g = int(start_color[1] * (1 - t) + end_color[1] * t)
     b = int(start_color[2] * (1 - t) + end_color[2] * t)
     return (r, g, b)
-
 def draw_object(position):
-    distance = math.sqrt(position[0]**2 + position[1]**2 + position[2]**2)
-    object_size = OBJECT_SIZE / (distance / FOCAL_LENGTH)  
+    x = position[0]**2 + position[1]**2 + position[2]**2
+    #distance = math.sqrt(x)
+    distance = InverSquareRoot([x])[0]
+    object_size = FOCAL_LENGTH* OBJECT_SIZE * distance
     
-    t = min(distance / (FOCAL_LENGTH * 4), 1) 
+    t = min(1/distance / (FOCAL_LENGTH * 4), 1)
     object_color = interpolate_color(OBJECT_COLOR_START, OBJECT_COLOR_END, t)
 
     if position[2] + FOCAL_LENGTH != 0: 
@@ -43,7 +98,6 @@ def draw_object(position):
 
     if 0 <= object_pos_x < WIDTH and 0 <= object_pos_y < HEIGHT: 
         pygame.draw.circle(screen, object_color, (int(object_pos_x), int(object_pos_y)), int(object_size))
-
 def main():
     running = True
     while running:
